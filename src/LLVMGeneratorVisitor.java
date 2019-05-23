561d38cc595ff7f9466f151b11350c6ea0019f55
Konstantinos Tsiaras <tsiaraskon@gmail.com>
2019-05-23 23:07:54 +0300

    llvm_helper_methods

--- src/LLVMGeneratorVisitor.java
+++ src/LLVMGeneratorVisitor.java
@@ -34,6 +34,29 @@ public class LLVMGeneratorVisitor extends GJDepthFirst<String, SymbolTable>{
         }
     }
 
+    void emit_llvm_helper_methods() {
+        String buffer = "\n" +
+            "declare i8* @calloc(i32, i32)\n" +
+            "declare i32 @printf(i8*, ...)\n" +
+            "declare void @exit(i32)\n" +
+            "\n" +
+            "@_cint = constant [4 x i8] c\"%d\\0a\\00\"\n" +
+            "@_cOOB = constant [15 x i8] c\"Out of bounds\\0a\\00\"\n" +
+            "define void @print_int(i32 %i) {\n" +
+            "\t%_str = bitcast [4 x i8]* @_cint to i8*\n" +
+            "\tcall i32 (i8*, ...) @printf(i8* %_str, i32 %i)\n" +
+            "\tret void\n" +
+            "}\n" +
+            "\n" +
+            "define void @throw_oob() {\n" +
+            "\t%_str = bitcast [15 x i8]* @_cOOB to i8*\n" +
+            "\tcall i32 (i8*, ...) @printf(i8* %_str)\n" +
+            "\tcall void @exit(i32 1)\n" +
+            "\tret void\n" +
+            "}\n";
+        emit(buffer);
+    }
+
 
     /**
      * f0 -> MainClass()
@@ -41,10 +64,9 @@ public class LLVMGeneratorVisitor extends GJDepthFirst<String, SymbolTable>{
      * f2 -> <EOF>
      */
     public String visit(Goal n, SymbolTable symbolTable) throws Exception {
-//        n.f0.accept(this, argu);
-//        n.f1.accept(this, argu);
-//        n.f2.accept(this, argu);
-        emit("Hello World!");
+        emit_llvm_helper_methods();
+        n.f0.accept(this, symbolTable);
+        n.f1.accept(this, symbolTable);
         return null;
     }
 
